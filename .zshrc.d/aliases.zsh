dde60033a98e90cc7613f58983abaa3070bf4e57
Jonathan Pliske <jonpliske@gmail.com>
2015-11-29 10:14:56 -0600

    removes old aliases

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -8,14 +8,6 @@ alias gcp="g cherry-pick"
 alias gdi="g diff"
 alias gdic="g diff --cached"
 
-alias be="bundle exec"
-
 alias curli="curl -s -i"
-
-alias ohmy="st ~/.oh-my-zsh"
-alias zshrc="st ~/.zshrc"
-
-# groupons
-alias gprep='be rake db:drop && be rake db:create && SKIP_OBSERVERS=true be rake db:migrate && be rake db:test:prepare && be rake spec:fixture_builder:rebuild && be rake db:seed && be rake assets:all:rebuild'
 
 alias hls="hadoop fs -ls"
