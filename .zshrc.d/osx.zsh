00de002fbe2ecde845e7b4634d5c710d56d6892a
Jon Pliske <jpliske@groupon.com>
2015-12-09 16:04:44 -0600

    iterm2 shell integration

--- .zshrc.d/osx.zsh
+++ .zshrc.d/osx.zsh
@@ -4,7 +4,7 @@ fi
 
 [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
-source ~/.iterm2_shell_integration.`basename $SHELL`
+[[ -s ~/.iterm2_shell_integration.zsh ]] && . ~/.iterm2_shell_integration.zsh
 
 if which jenv &> /dev/null; then
   export PATH="$HOME/.jenv/bin:$PATH"
