e5e433b37c70e8040b051f342ce77b88c68512f3
Jonathan Pliske <jonpliske@gmail.com>
2016-12-31 14:48:18 -0600

    fancy prompt checker

--- .zshrc.d/prompt.zsh
+++ .zshrc.d/prompt.zsh
@@ -1,10 +1,28 @@
 autoload -Uz promptinit && promptinit
 
+_check_zsh_prompt() {
+  local prompt=$1
+  local prompt_output=`prompt -l| tail -n1`
+  local avail=("${(@s/ /)prompt_output}")
+
+  if [[ ${avail[(i)$prompt]} -le ${#avail} ]]; then
+    echo $prompt
+    return 0
+  fi
+
+  return 1
+}
+
 if [ "#TERM" ]; then
     case $TERM in
     xterm*)
+        prompt_check=$( _check_zsh_prompt pure )
+      if [[ -n prompt_check ]]; then
         prompt pure
-        ;;
+      else
+        prompt redhat
+      fi
+      ;;
     linux*)
         prompt redhat
         ;;
