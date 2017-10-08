01dc7c85a717b2e24cff67efaa552a0bd96c6559
Jonathan Pliske <jonpliske@gmail.com>
2017-10-08 11:44:31 -0500

    more emacs

--- init.el
+++ init.el
@@ -1,101 +1,280 @@
-(require 'package)
+(setq user-full-name "Jonathan Pliske")
+(setq user-mail-address "jonpliske@gmail.com")
 
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(setenv "PATH" (concat "/usr/local/bin:/usr/bin:/bin"
+		       (getenv "PATH")))
+(require 'cl)
 
+(load "package")
 (package-initialize)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(setq package-archive-enable-alist '(("melpa" deft magit)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(package-selected-packages
-   (quote
-    (elm-mode fiplr neotree python-mode python-switch-quotes yaml-mode org)))
- '(xterm-mouse-mode t)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+(defvar pliske/packages '(auto-complete
+			  autopair
+			  base16-theme
+			  clojure-mode
+			  deft
+			  erlang
+			  flycheck
+			  gist
+			  haskell-mode
+			  htmlize
+			  idris-mode
+			  magit
+			  markdown-mode
+			  nodejs-repl
+			  org
+			  paredit
+			  restclient
+			  ruby-mode
+			  scala-mode
+			  smex
+			  web-mode
+			  writegood-mode
+			  yaml-mode)
+  "Default packages")
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(defun pliske/packages-installed-p ()
+  (loop for pkg in pliske/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
 
-;; load custom themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(unless (pliske/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg pliske/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
 
-;; load neotree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
 
-;; no splash
-(setq inhibit-startup-screen t)
+(if (display-graphic-p)
+    (progn
+      (scroll-bar-mode -1)
+      (tool-bar-mode -1)
+      (menu-bar-mode -1)))
 
-;; linum mode
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+(setq make-backup-files nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+;; Vendor directory
+(defvar pliske/vendor-dir (expand-file-name "vendor" user-emacs-directory))
+(add-to-list 'load-path pliske/vendor-dir)
+
+(dolist (project (directory-files pliske/vendor-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+;; Mouse support
+(xterm-mouse-mode t)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mount 't)
+(setq scroll-step 1)
+(unless (display-graphic-p)
+  (progn
+    (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+    (global-set-key (kbd "<mouse-5>") 'scroll-up-line)))
+
+
+;; Org mode
+
+(setq org-log-done t
+      org-todo-keywords '((sequence "TODO" "INPROGRESS" "DONE"))
+      org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold))))
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (flyspell-mode)))
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (writegood-mode)))
+
+;; org-agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-agenda-show-log t
+      org-agenda-todo-ignore-scheduled t
+      org-agenda-todo-ignore-deadlines t)
+(setq org-agenda-files (list "~/Dropbox/org/personal.org"
+			     "~/Dropbox/org/groupon.org"))
+
+;; org-habit
+
+(require 'org)
+(require 'org-install)
+(require 'org-habit)
+(add-to-list 'org-modules "org-habit")
+(setq org-habit-preceding-days 7
+      org-habit-following-days 1
+      org-habit-graph-column 80
+      org-habit-show-habits-only-for-today t
+      org-habit-show-all-today t)
+
+
+;; org-babel
+
+(require 'ob)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sh . t)
+   (ditaa . t)
+   (plantuml . t)
+   (dot . t)
+   (ruby . t)
+   (js . t)
+   (C . t)
+   (haskell . t)))
+
+(add-to-list 'org-src-lang-modes (quote ("dot". graphviz-dot)))
+(add-to-list 'org-src-lang-modes (quote ("plantuml" . fundamental)))
+(add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
+
+(defvar org-babel-default-header-args:clojure
+  '((:results . "silent") (:tangle . "yes")))
+
+(defun org-babel-execute:clojure (body params)
+  (lisp-eval-string body)
+  "Done!")
+
+(provide 'ob-clojure)
+
+(setq org-src-fontify-natively t
+      org-confirm-babel-evaluate nil)
+
+(add-hook 'org-babel-after-execute-hook (lambda ()
+                                          (condition-case nil
+                                              (org-display-inline-images)
+                                            (error nil)))
+          'append)
+;; ditaa
+(setq org-ditaa-jar-path "~/.emacs.d/vendor/ditaa0_9.jar")
+
+;; plantUML
+(setq org-plantuml-jar-path "~/.emacs.d/vendor/plantuml.1.2017.16.jar")
+
+;; deft
+(setq deft-directory "~/Dropbox/deft")
+(setq deft-use-filename-as-title t)
+(setq deft-extension "org")
+(setq deft-text-mode 'org-mode)
+
+;; Smex
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Ido
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+;; Line and column numbers
+(setq column-number-mode t)
 (global-linum-mode t)
 (setq linum-format "%3d ")
 
-;; hide menu
-(tool-bar-mode -1)
+;; Temporary files - who needs em?
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-;; make backup to a designated dir, mirroring the full path
+;; autopair-mode
+(require 'autopair)
 
-(defun my-backup-file-name (fpath)
-  "Return a new file path of a given file path.
-If the new path's directories does not exist, create them."
-  (let* (
-        (backupRootDir "~/.emacs.d/emacs-backup/")
-        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
-        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
-        )
-    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
-    backupFilePath
-  )
-)
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
 
-(setq make-backup-file-name-function 'my-backup-file-name)
-
-;; bind C-x f to fiplr fuzzy search
-(global-set-key (kbd "C-x f") 'fiplr-find-file)
-
-;; delete line under cursor
-(defun delete-line()
+;; Indentation and cleanup
+(defun untabify-buffer ()
   (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  )
+  (untabify (point-min) (point-max)))
 
-
-;; duplicate line
-(defun duplicate-line()
+(defun indent-buffer ()
   (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (open-line 1)
-  (next-line 1)
-  (yank)
-  )
+  (indent-region (point-min) (point-max)))
 
-(global-set-key (kbd "C-d") 'duplicate-line)
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
 
-;; customize babel languages
-(Org-babel-do-load-languages
- 'org-babel-load-languages
- '((sh . t) (python . t) (ruby . t)
-   ))
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+(setq-default show-trailing-whitespace t)
+
+
+;; Language hooks
+
+;; YAML Mode
+
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+
+;; JavaScript Mode
+
+(defun js-custom ()
+  "js-mode-hook"
+  (setq js-indent-level 2))
+
+(add-hook 'js-mode-hook 'js-custom)
+
+
+;; Markdown Mode
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-hook 'markdown-mode-hook
+	  (lambda ()
+	    (visual-line-mode t)
+	    (writegood-mode t)
+	    (flyspell-mode t)))
+(setq markdown-command "pandoc --smart -f -markdown -t html")
+(setq markdown-css-paths `(,(expand-file-name "markdown.css" pliske/vendor-dir)))
+
+;;(setq base16-theme-use-shell-colors t)
+;;(setq base16-theme-color-source "colors")
+;;(load-theme 'base16-default-dark t)
+;;(load-theme 'wombat t)
+
