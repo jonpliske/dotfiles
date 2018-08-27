a6597a4a9e8a75bd1b54ebc320d1c1f50375604d
Jon Pliske <jpliske@groupon.com>
2018-08-27 14:08:16 -0500

    fixes gpg-agent 2.1+ ssh auth on macos

--- zshrc.d/gpg-agent.zsh
+++ zshrc.d/gpg-agent.zsh
@@ -1,12 +1,9 @@
 if [[ $(uname) == "Darwin" ]]; then
-
-  if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-      source ~/.gnupg/.gpg-agent-info
-      export GPG_AGENT_INFO
-      export SSH_AUTH_SOCK
-      export SSH_AGENT_PID
+  if [ -f ~/.gnupg/S.gpg-agent.ssh ] && [ -n "$(pgrep gpg-agent)" ]; then
+    export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
   else
-      eval $(gpg-agent --quiet --debug-level 0 --daemon --log-file /dev/null)
+    gpgconf --launch gpg-agent
+    export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
   fi
 else
   export GPG_TTY=$(tty)
