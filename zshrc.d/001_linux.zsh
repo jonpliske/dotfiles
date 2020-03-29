43c1da9f075691efffbd775649594f1a0bd3a688
Jonathan Pliske <jonpliske@gmail.com>
2020-03-29 09:55:02 -0500

    pbcopy/pbpaste /w xsel

--- zshrc.d/001_linux.zsh
+++ zshrc.d/001_linux.zsh
@@ -2,3 +2,8 @@ cmd=thefuck
 if (( $+commands[$cmd] )) ; then
   eval $(thefuck --alias)
 fi
+
+if command -v xsel 1>/dev/null 2>&1; then
+    alias pbcopy=xsel -i -b
+    alias pbpaste=xsel -o -b
+fi
