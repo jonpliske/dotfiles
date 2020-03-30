b479735c796334d358ae0a7c98ae2cfdbdbf5f03
Jonathan Pliske <jonpliske@gmail.com>
2020-03-30 18:46:27 -0500

    fixes ordering of pyenv init

--- zshrc.d/python.zsh
+++ zshrc.d/python.zsh
@@ -1,9 +1,10 @@
 export PYENV_ROOT="$HOME/.pyenv"
 
+typeset -U path
+path=($PYENV_ROOT/bin $path[@])
+
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
 fi
 
-typeset -U path
-path=($PYENV_ROOT/bin $path[@])
