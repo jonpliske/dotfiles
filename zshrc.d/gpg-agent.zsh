7825b3e2254e526037ac1d766b7da3a3b9bab013
Jonathan Pliske <jonpliske@gmail.com>
2020-03-30 23:17:21 -0500

    parse QUBES_GPG_DOMAIN from qubes service fs

--- zshrc.d/gpg-agent.zsh
+++ zshrc.d/gpg-agent.zsh
@@ -1,3 +1,12 @@
+# handle split-gpg for qubes os
+# https://www.qubes-os.org/doc/split-gpg/
+files=(/var/run/qubes-service/split-gpg*(N))
+if ((#files)); then
+    service=$(basename $files[-1])
+    domain=(${(s/-/)service})
+    export QUBES_GPG_DOMAIN=${(j:-:)domain[3,-1]}
+fi
+
 if [[ $(uname) == "Darwin" ]]; then
   if [ -f ~/.gnupg/S.gpg-agent.ssh ] && [ -n "$(pgrep gpg-agent)" ]; then
     export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
