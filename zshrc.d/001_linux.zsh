19eecdac1380d724a7b142b56328b0a8ea31dabd
Jonathan Pliske <jonpliske@gmail.com>
2020-04-01 10:53:21 -0500

    adds aliases for dmesg/systemctl/journalctl

--- zshrc.d/001_linux.zsh
+++ zshrc.d/001_linux.zsh
@@ -7,3 +7,13 @@ if command -v xsel 1>/dev/null 2>&1; then
     alias pbcopy="xsel -i -b"
     alias pbpaste="xsel -o -b"
 fi
+
+alias dmesgh="dmesg -eH"
+alias dmesgtail="dmesgh -w"
+
+alias sctl=systemctl
+alias sctls="systemctl status"
+alias sctlstart="sudo systemctl start"
+alias sctlstop="sudo systemctl stop"
+
+alias jctl=journalctl
