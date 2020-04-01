1e523efdd6c279b66c896d89da29b226a2c4a2f9
Jonathan Pliske <jonpliske@gmail.com>
2020-04-01 08:59:31 -0500

    fix pbcopy/pbpaste aliases

--- zshrc.d/001_linux.zsh
+++ zshrc.d/001_linux.zsh
@@ -4,6 +4,6 @@ if (( $+commands[$cmd] )) ; then
 fi
 
 if command -v xsel 1>/dev/null 2>&1; then
-    alias pbcopy=xsel -i -b
-    alias pbpaste=xsel -o -b
+    alias pbcopy="xsel -i -b"
+    alias pbpaste="xsel -o -b"
 fi
