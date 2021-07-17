bfa9bacec2a8fe5b59d0e95ebd496ad2d8fa9b27
Jonathan Pliske <jonpliske@gmail.com>
2021-07-17 18:23:41 -0500

    Fixes utils.map for nvim-lua

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -12,7 +12,7 @@ end
 
 function utils.map(mode, lhs, rhs, opts)
   local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opt) end
+  if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
