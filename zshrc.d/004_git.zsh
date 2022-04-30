a07be468bea9c729e5571595e44cd81714137d07
Jonathan Pliske <jonpliske@gmail.com>
2022-04-29 18:20:05 -0700

    Adds gcm alias+function /w main support

--- zshrc.d/004_git.zsh
+++ zshrc.d/004_git.zsh
@@ -3,7 +3,7 @@ git_clone_take () {
   local repo_url=$1
 
 
-  if ! git clone $repo_url; then
+  if ! git clone $repo_url >&2; then
     echo "Unable to clone $repo_url" >&2
     return 1
   fi
@@ -16,4 +16,16 @@ git_clone_take () {
   fi
 }
 
+git_checkout_main_or_fallback () {
+    local default=$(git config init.defaultBranch)
+    if ! git checkout main >&2; then
+        if ! git checkout master >&2; then
+            echo "no suitable main branch" >&2
+            return 1
+        fi
+    fi
+
+}
+
 alias gct=git_clone_take
+alias gcm=git_checkout_main_or_fallback
